a4b3a5526cfcc7925ed1cc91fc382bf4a8c100b7
rakowsm <77644739+rakowsm@users.noreply.github.com>
2021-01-18 22:51:58 +0100

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;        
+
+    public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+    private int level = 1;
+
+    void Awake()
+    {
+        
+        if (instance == null)
+            instance = this;
+
+        
+        else if (instance != this) 
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+
+    }
+    void Update()
+    {
+
+    }
+}
